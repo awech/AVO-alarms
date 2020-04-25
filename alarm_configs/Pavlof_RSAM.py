2893eb9298e79d7a299b3a293dc85eb5662cc187
awech <awech@usgs.gov>
2020-04-25 21:55:20 +0000

    just testing different parameters

--- alarm_configs/Pavlof_RSAM.py
+++ alarm_configs/Pavlof_RSAM.py
@@ -19,3 +19,5 @@ min_sta   = 3    # minimum number of stations for detection
 taper_val = 5 	 # seconds to taper beginning and end of trace before filtering
 f1		  = 1.0  # minimum frequency for bandpass filter
 f2		  = 5.0  # maximum frequency for bandpass filter
+
+plot_duration=3600

34d01f9262ceafb2c9c2ea160af4302ea191c9e0
awech <awech@usgs.gov>
2020-04-25 01:52:54 +0000

    renamed lightning config file

---
+++ alarm_configs/Lightning_config.py
@@ -0,0 +1,13 @@
+alarm_type = 'Lightning_new'
+alarm_name = 'Lightning_new' 
+
+outfile    = 'alarm_aux_files/Lightning_last.txt'
+
+# dist1 = 20			# distance for inner ring (in km)
+# dist2 = 100			# distance for outer ring (in km)
+dist1 = 50			# distance for inner ring (in km)
+dist2 = 200			# distance for outer ring (in km)
+
+duration = 60*60	# time limit in seconds for remembering strokes
+
+# mattermost_channel_id = 'bbee81rp13n68mstu33xf3b88a'

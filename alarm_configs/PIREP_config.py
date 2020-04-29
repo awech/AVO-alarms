100ff9540a13b7204ef782d9c8e8f0f7a9bfcb9d
awech <awech@usgs.gov>
2020-04-29 22:17:58 +0000

    renamed PIREP and NOAA_CIMSS config files

--- alarm_configs/PIREP_config.py
+++ alarm_configs/PIREP_config.py
@@ -6,7 +6,7 @@ tmp_zipped_dir = 'alarm_aux_files/tmp_zipped_dir'
 outfile        = 'alarm_aux_files/PIREPs.txt'
 
 max_distance  = 200		# maximum distance for sending an alert
-cron_interval = 60*60	# time window of data to grab before present
+duration 	  = 60*60	# time limit in seconds for remembering strokes
 non_urgent    = False	# send notification if classified as non-urgent or not
 
 mattermost_channel_id = 'fdde17wkrfdqze785wt69mrqeo'

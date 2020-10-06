7cdda02203d17e4f9387c8f6d53ee89e6c00e0b8
awech <awech@usgs.gov>
2020-10-06 20:50:53 +0000

    new gain factor

--- alarm_configs/SDPI_Infrasound_config.py
+++ alarm_configs/SDPI_Infrasound_config.py
@@ -32,7 +32,7 @@ taper_val = 5.0  # seconds to taper beginning and end of trace before filtering
 f1		  = 1.0  # minimum frequency for bandpass filter
 f2		  = 10.0 # maximum frequency for bandpass filter
 
-digouti   = (1/419430.0)/(1e-2)	# convert counts to Pressure in Pa (Q330 + VDP-10 mics)
+digouti   = (1/419430.0)/(0.0275)	# convert counts to Pressure in Pa (Q330 + (VDP-10 pre Oct 15 2019) Chap Vx2 (post Oct 15 2019) mics)
 min_cc    = 0.6					# min normalized correlation coefficient to accept
 min_chan  = 3					# minimum # of channels for code to run
 cc_shift_length = 3*50			# maximum samples to shift in cross-correlation (usually at 50 sps)

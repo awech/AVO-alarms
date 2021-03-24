2069b4300a5bfff441df396b1a812025c2af708e
awech <awech@usgs.gov>
2021-03-24 20:37:30 +0000

    threshold adjustments

--- alarm_configs/KENI_Infrasound_config.py
+++ alarm_configs/KENI_Infrasound_config.py
@@ -16,22 +16,22 @@ SCNL=[
 # Azimuthal tolerance is in degrees
 # seismic_scnl is a list of seismic channels to be plotted with infrasound on detect
 VOLCANO=[
- {'volcano': 'Spurr',  'v_lat': 61.29897,  'v_lon': -152.25122,  'Azimuth_tolerance': 5, 'min_pa': 0.1, 'vmin':0.28, 'vmax':0.4,
+ {'volcano': 'Spurr',  'v_lat': 61.29897,  'v_lon': -152.25122,  'Azimuth_tolerance': 5, 'min_pa': 1.0, 'vmin':0.28, 'vmax':0.4,
      'seismic_scnl': ['SPCP.BHZ.AV.--','SPCL.BHZ.AV.--','SPU.BHZ.AV.--']},
 
- {'volcano': 'Redoubt',  'v_lat': 60.48576,  'v_lon': -152.74282,  'Azimuth_tolerance': 5, 'min_pa': 0.1, 'vmin':0.28, 'vmax':0.4,
+ {'volcano': 'Redoubt',  'v_lat': 60.48576,  'v_lon': -152.74282,  'Azimuth_tolerance': 5, 'min_pa': 1.0, 'vmin':0.28, 'vmax':0.4,
      'seismic_scnl': ['RDDF.BHZ.AV.--','RDDF.BHZ.AV.--','RDSO.BHZ.AV.--']},
 
-{'volcano': 'Iliamna',  'v_lat': 60.03220,  'v_lon': -153.09002,  'Azimuth_tolerance': 5, 'min_pa': 0.1, 'vmin':0.28, 'vmax':0.4,
+{'volcano': 'Iliamna',  'v_lat': 60.03220,  'v_lon': -153.09002,  'Azimuth_tolerance': 5, 'min_pa': 0.8, 'vmin':0.28, 'vmax':0.4,
     'seismic_scnl': ['ILSW.BHZ.AV.--','ILS.BHZ.AV.--','IVE.BHZ.AV.--']},
 
- {'volcano': 'Augustine',  'v_lat': 59.36107,  'v_lon': -153.42938,  'Azimuth_tolerance': 8, 'min_pa': 0.1, 'vmin':0.28, 'vmax':0.4,
+ {'volcano': 'Augustine',  'v_lat': 59.36107,  'v_lon': -153.42938,  'Azimuth_tolerance': 5, 'min_pa': 1.0, 'vmin':0.28, 'vmax':0.4,
      'seismic_scnl': ['AUJA.BHZ.AV.--','AUJK.BHZ.AV.--','AUSS.BHZ.AV.--']},
 
- {'volcano': 'Fourpeaked', 'v_lat': 58.7625, 'v_lon': -153.6632,   'Azimuth_tolerance': 5, 'min_pa': 0.1, 'vmin':0.28, 'vmax':0.4,
+ {'volcano': 'Fourpeaked', 'v_lat': 58.7625, 'v_lon': -153.6632,   'Azimuth_tolerance': 5, 'min_pa': 0.5, 'vmin':0.28, 'vmax':0.4,
      'seismic_scnl': ['Q19K.BHZ.AV.--','KARR.BHZ.AV.--','KAHG.BHZ.AV.--']},
 
- {'volcano': 'Katmai', 'v_lat': 58.263132, 'v_lon': -155.148067,   'Azimuth_tolerance': 10, 'min_pa': 0.1, 'vmin':0.28, 'vmax':0.4,
+ {'volcano': 'Katmai', 'v_lat': 58.263132, 'v_lon': -155.148067,   'Azimuth_tolerance': 10, 'min_pa': 0.5, 'vmin':0.28, 'vmax':0.4,
      'seismic_scnl': ['MGLS.BHZ.AV.--','KABU.BHZ.AV.--','ACH.BHZ.AV.--']},
 ]
 

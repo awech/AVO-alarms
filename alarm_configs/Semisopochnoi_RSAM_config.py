1ad68af968ebdb76f5046ce0f9e8329f3e807fe3
awech <awech@usgs.gov>
2021-09-24 19:27:49 +0000

    added more stations

--- alarm_configs/Semisopochnoi_RSAM_config.py
+++ alarm_configs/Semisopochnoi_RSAM_config.py
@@ -4,10 +4,11 @@ alarm_name = 'Semisopochnoi RSAM'	# this is the alarm name sent to icinga and in
 # Stations list. Last station is arrestor.
 SCNL=[
 {'scnl':'CEAP.BHZ.AV.--'	, 'value':     475		},
-# {'scnl':'CEPE.SHZ.AV.--'	, 'value':  500 * 1e3		}, # removed Feb 28, 2020. Dead channel.
 {'scnl':'CEPE.BHZ.AV.--'	, 'value':     600		},
 {'scnl':'CESW.BHZ.AV.--'	, 'value':     700		},
 {'scnl':'CETU.BHZ.AV.--'	, 'value':     475		},
+{'scnl':'CERB.BHZ.AV.--'	, 'value':     1000		},
+{'scnl':'CERA.BHZ.AV.--'	, 'value':     475		},
 {'scnl':'AMKA.BHZ.AV.--'	, 'value':     100		}, # arrestor station
 ]
 

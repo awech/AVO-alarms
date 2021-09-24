7e8e2d3f94b2da3fa29f2261d8a2ed37ac95689c
awech <awech@usgs.gov>
2021-09-24 19:26:55 +0000

    new broadband channels

--- alarm_configs/Okmok_RSAM_config.py
+++ alarm_configs/Okmok_RSAM_config.py
@@ -3,17 +3,28 @@ alarm_name = 'Okmok RSAM'	# this is the alarm name sent to icinga and in message
 
 # Stations list. Last station is arrestor. mmh
 
+# SCNL=[
+# {'scnl':'OKER.EHZ.AV.--'	, 'value': 400		},
+# {'scnl':'OKNC.BHZ.AV.--'	, 'value': 400		},
+# {'scnl':'OKTU.EHZ.AV.--'	, 'value': 400		},
+# {'scnl':'OKWE.EHZ.AV.--'	, 'value': 400		},
+# {'scnl':'MREP.EHZ.AV.--'	, 'value': 100		}, # arrestor station
+# ]
+
 SCNL=[
-{'scnl':'OKER.EHZ.AV.--'	, 'value': 400		},
-{'scnl':'OKNC.BHZ.AV.--'	, 'value': 400		},
-{'scnl':'OKTU.EHZ.AV.--'	, 'value': 400		},
-{'scnl':'OKWE.EHZ.AV.--'	, 'value': 400		},
-{'scnl':'MREP.EHZ.AV.--'	, 'value': 100		}, # arrestor station
+{'scnl':'OKCF.BHZ.AV.--'	, 'value': 675		},
+{'scnl':'OKNO.BHZ.AV.--'	, 'value': 375		},
+{'scnl':'OKBR.BHZ.AV.--'	, 'value': 1225		},
+{'scnl':'OKFG.BHZ.AV.--'	, 'value': 300		},
+{'scnl':'OKTU.BHZ.AV.--'	, 'value': 550		},
+{'scnl':'OKNC.BHZ.AV.--'	, 'value': 775		},
+{'scnl':'OKCE.BHZ.AV.--'	, 'value': 675		},
+{'scnl':'MGOD.BHZ.AV.--'	, 'value': 100		}, # arrestor station
 ]
 
 duration  = 5*60 # duration value in seconds
 latency   = 10   # seconds between timestamps and end of data window
-min_sta   = 3    # minimum number of stations for detection
+min_sta   = 4    # minimum number of stations for detection
 taper_val = 5 	 # seconds to taper beginning and end of trace before filtering
 f1		  = 1.0  # minimum frequency for bandpass filter
 f2		  = 5.0  # maximum frequency for bandpass filter

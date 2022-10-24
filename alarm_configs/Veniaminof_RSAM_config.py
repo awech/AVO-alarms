94cd526c4fdde01b274905939ad6f1757f60d689
awech <awech@usgs.gov>
2022-10-24 19:05:11 +0000

    assigned new values following A2D

--- alarm_configs/Veniaminof_RSAM_config.py
+++ alarm_configs/Veniaminof_RSAM_config.py
@@ -3,18 +3,18 @@ alarm_name = 'Veniaminof RSAM'	# this is the alarm name sent to icinga and in me
 
 # Stations list. Last station is arrestor.
 SCNL=[
-{'scnl':'VNCG.BHZ.AV.--'	, 'value': 3000		}, # changed 29-Oct-2018 to avoid network-wide noise spikes
-{'scnl':'VNSW.BHZ.AV.--'	, 'value': 3000		},
-{'scnl':'VNWF.BHZ.AV.--'	, 'value': 3000		},
-{'scnl':'VNKR.BHZ.AV.--'	, 'value': 3000		},
-{'scnl':'VNSO.BHZ.AV.--'	, 'value': 3000		},
-{'scnl':'VNSG.BHZ.AV.--'	, 'value': 3000		},
+{'scnl':'VNCG.BHZ.AV.--'	, 'value': 500	}, # changed 29-Oct-2018 to avoid network-wide noise spikes
+{'scnl':'VNSW.BHZ.AV.--'	, 'value': 200	},
+{'scnl':'VNWF.BHZ.AV.--'	, 'value': 300	},
+{'scnl':'VNKR.BHZ.AV.--'	, 'value': 200	},
+{'scnl':'VNSO.BHZ.AV.--'	, 'value': 250	},
+{'scnl':'VNSG.BHZ.AV.--'	, 'value': 200	},
 {'scnl':'SDPT.BHZ.AT.--'	, 'value': 200	}, # arrestor station
 ]
 
 duration  = 5*60 # duration value in seconds
 latency   = 10   # seconds between timestamps and end of data window
-min_sta   = 7    # minimum number of stations for detection
+min_sta   = 4    # minimum number of stations for detection
 taper_val = 5 	 # seconds to taper beginning and end of trace before filtering
 f1		  = 1.0  # minimum frequency for bandpass filter
 f2		  = 5.0  # maximum frequency for bandpass filter

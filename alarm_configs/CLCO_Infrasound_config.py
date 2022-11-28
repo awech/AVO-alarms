c53cb1af53ff7633542080b4ab74808b376f4a21
awech <awech@usgs.gov>
2022-11-28 20:11:43 +0000

    changed BDF to HDF

--- alarm_configs/CLCO_Infrasound_config.py
+++ alarm_configs/CLCO_Infrasound_config.py
@@ -8,12 +8,12 @@ SCNL=[
 # {'scnl':'CLCO3.BDF.AV.--'	, 'sta_lat': 52.7874600000	, 'sta_lon': -169.7210166667},
 # {'scnl':'CLCO4.BDF.AV.--'	, 'sta_lat': 52.7861266667	, 'sta_lon': -169.7203966667},
 # {'scnl':'CLCO5.BDF.AV.--'	, 'sta_lat': 52.7851866667	, 'sta_lon': -169.7250066667},
-    {'scnl':'CLCO.BDF.AV.01'    , 'sta_lat': 52.7864125000  , 'sta_lon': -169.7229250000},
-    {'scnl':'CLCO.BDF.AV.02'    , 'sta_lat': 52.7871866667  , 'sta_lon': -169.7244333330},
-    {'scnl':'CLCO.BDF.AV.03'    , 'sta_lat': 52.7874600000  , 'sta_lon': -169.7210166667},
-    {'scnl':'CLCO.BDF.AV.04'    , 'sta_lat': 52.7861266667  , 'sta_lon': -169.7203966667},
-    {'scnl':'CLCO.BDF.AV.05'    , 'sta_lat': 52.7851866667  , 'sta_lon': -169.7250066667},
-    {'scnl':'CLCO.BDF.AV.06'    , 'sta_lat': 52.785861      , 'sta_lon': -169.723179    },
+    {'scnl':'CLCO.HDF.AV.01'    , 'sta_lat': 52.7864125000  , 'sta_lon': -169.7229250000},
+    {'scnl':'CLCO.HDF.AV.02'    , 'sta_lat': 52.7871866667  , 'sta_lon': -169.7244333330},
+    {'scnl':'CLCO.HDF.AV.03'    , 'sta_lat': 52.7874600000  , 'sta_lon': -169.7210166667},
+    {'scnl':'CLCO.HDF.AV.04'    , 'sta_lat': 52.7861266667  , 'sta_lon': -169.7203966667},
+    {'scnl':'CLCO.HDF.AV.05'    , 'sta_lat': 52.7851866667  , 'sta_lon': -169.7250066667},
+    {'scnl':'CLCO.HDF.AV.06'    , 'sta_lat': 52.785861      , 'sta_lon': -169.723179    },
 ]
 
 # Volcano list to be monitored

e70ad93a9fb160b2ec2725e27b8f4402ef2b9244
awech <awech@usgs.gov>
2023-03-03 00:21:18 +0000

    changing arrestor from KIMD to ETKA and lowering other thresholds

--- alarm_configs/Takawangha_RSAM_config.py
+++ alarm_configs/Takawangha_RSAM_config.py
@@ -5,12 +5,14 @@ alarm_name = 'Takawangha RSAM'	# this is the alarm name sent to icinga and in me
 #													    M2.8 (AEC)
 SCNL=[
 {'scnl':'TANO.BHZ.AV.--'	, 'value':  4.8 * 1e3	}, # 4.7k
-{'scnl':'TASO.BHZ.AV.--'	, 'value':  8   * 1e3	}, # 7.9k
-{'scnl':'TASE.BHZ.AV.--'	, 'value':  38  * 1e3	}, # 37.5k
-{'scnl':'TAFP.BHN.AV.--'	, 'value':  34  * 1e3	}, # 33.7k
+{'scnl':'TASO.BHZ.AV.--'	, 'value':  6   * 1e3	}, # 7.9k
+{'scnl':'TASE.BHZ.AV.--'	, 'value':  30  * 1e3	}, # 37.5k
+{'scnl':'TAFP.BHN.AV.--'	, 'value':  30  * 1e3	}, # 33.7k
 {'scnl':'TAPA.BHZ.AV.--'	, 'value':  6.5 * 1e3	}, # 6.5k
 {'scnl':'TAFL.BHZ.AV.--'	, 'value':  3.9 * 1e3	}, # 3.8k 
-{'scnl':'KIMD.BHZ.AV.--'	, 'value':  0.3 * 1e3	}, # 0.17k arrestor station 
+# {'scnl':'KIMD.BHZ.AV.--'	, 'value':  0.3 * 1e3	}, # 0.17k arrestor station 
+# {'scnl':'ADAG.BHZ.AV.--'	, 'value':  0.3 * 1e3	}, # 0.17k arrestor station 
+{'scnl':'ETKA.BHZ.AV.--'	, 'value':  0.3 * 1e3	}, # 0.17k arrestor station 
 ]
 
 duration  = 5*60 # duration value in seconds

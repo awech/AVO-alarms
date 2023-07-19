173b6a841914d00b94dff09cee716b6c492b7656
awech <awech@usgs.gov>
2023-07-19 23:35:48 +0000

    new Shishaldin eruption parameters July 2023

--- alarm_configs/Shishaldin_RSAM_config.py
+++ alarm_configs/Shishaldin_RSAM_config.py
@@ -1,13 +1,22 @@
 alarm_type = 'RSAM'				# this designates which alarm module will be imported and executed
 alarm_name = 'Shishaldin RSAM'	# this is the alarm name sent to icinga and in message alerts
 
+# # Stations list. Last station is arrestor.
+# SCNL=[
+# {'scnl':'SSLN.BHZ.AV.--'	, 'value': 4000		},
+# {'scnl':'SSLS.BHZ.AV.--'	, 'value': 20000	},
+# {'scnl':'SSBA.BHZ.AV.--'	, 'value': 700		},
+# {'scnl':'ISNN.SHZ.AV.--'	, 'value': 1100		},
+# {'scnl':'DTN.BHZ.AV.--'	, 'value': 250		}, # arrestor station
+# ]
+
 # Stations list. Last station is arrestor.
 SCNL=[
-{'scnl':'SSLN.BHZ.AV.--'	, 'value': 4000		},
-{'scnl':'SSLS.BHZ.AV.--'	, 'value': 20000	},
-{'scnl':'SSBA.BHZ.AV.--'	, 'value': 700		},
-{'scnl':'ISNN.SHZ.AV.--'	, 'value': 1100		},
-{'scnl':'DTN.BHZ.AV.--'	, 'value': 250		}, # arrestor station
+# {'scnl':'SSLN.BHZ.AV.--'	, 'value': 4000		},
+# {'scnl':'SSLS.BHZ.AV.--'	, 'value': 20000	},
+{'scnl':'SSBA.BHZ.AV.--'	, 'value': 600		},
+{'scnl':'ISNN.BHZ.AV.--'	, 'value': 290		},
+{'scnl':'BLDW.BHZ.AV.--'	, 'value': 250		}, # arrestor station
 ]
 
 duration  = 5*60 # duration value in seconds

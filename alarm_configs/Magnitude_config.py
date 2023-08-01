f09c6f69ab2e5074ffda61918d085dd6be0693d3
awech <awech@usgs.gov>
2023-08-01 17:33:02 +0000

    remove Tana/Taka and added Katmai cluster

--- alarm_configs/Magnitude_config.py
+++ alarm_configs/Magnitude_config.py
@@ -12,9 +12,10 @@ DURATION = 2*3600
 mattermost_channel_id = '1yxp3dhsjtd55jbbrawddzkk6h'
 
 mm_response_channels = {
-	'Tanaga': 'dcimdcioh7g8tpywkaccf1uruc',
-	'Takawangha': 'dcimdcioh7g8tpywkaccf1uruc',
 	'Aniakchak': 'eoeizwgp8bdnpmqpcicb7tjqbw',
 	'Trident': 'd4e8jhmay7rbfe19za1zywwhpe',
 	'Katmai': 'd4e8jhmay7rbfe19za1zywwhpe',
+	'Martin': 'd4e8jhmay7rbfe19za1zywwhpe',
+	'Mageik': 'd4e8jhmay7rbfe19za1zywwhpe',
+	'Novarupta': 'd4e8jhmay7rbfe19za1zywwhpe',
 }

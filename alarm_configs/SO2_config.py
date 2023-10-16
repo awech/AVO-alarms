2e822e4cdc21a2e9f0bdf820906233e5e92ddd90
awech <awech@usgs.gov>
2023-10-16 23:25:17 +0000

    changed image download and plotting

--- alarm_configs/SO2_config.py
+++ alarm_configs/SO2_config.py
@@ -3,6 +3,7 @@ alarm_name = 'SO2'
 
 volc_file = 'alarm_aux_files/volcano_list.xlsx'
 outfile    = 'alarm_aux_files/SO2_last.txt'
+img_file  = 'alarm_aux_files/sacs_out_.png'
 
 max_distance  = 200		# maximum distance for sending an alert
 mattermost_channel_id = 'qkkbtrmbi7nijed9457gsrix6o'

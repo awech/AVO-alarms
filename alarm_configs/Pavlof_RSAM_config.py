ede6a814f238285d1b978526a78417b526a1225c
awech <awech@usgs.gov>
2023-12-28 00:40:00 +0000

    updated PVV channel and levels

--- alarm_configs/Pavlof_RSAM_config.py
+++ alarm_configs/Pavlof_RSAM_config.py
@@ -4,20 +4,20 @@ alarm_name = 'Pavlof RSAM'		# this is the alarm name sent to icinga and in messa
 # Stations list. Last station is arrestor.
 SCNL=[
 {'scnl':'PS4A.BHZ.AV.--'	, 'value': 400	},
-{'scnl': 'PVV.SHZ.AV.--'	, 'value': 1500	},
+{'scnl': 'PVV.BHZ.AV.--'	, 'value': 400	},
 {'scnl':'PS1A.BHZ.AV.--'	, 'value': 375	},
 {'scnl': 'HAG.SHZ.AV.--'	, 'value': 1225	},
 {'scnl':'PN7A.BHZ.AV.--'	, 'value': 450	},
-{'scnl':'PV6A.BHZ.AV.--'	, 'value': 1225	},
+{'scnl':'PV6A.BHZ.AV.--'	, 'value': 325	},
 # {'scnl':'FALS.BHZ.AK.--'	, 'value': 200	}, # arrestor station, out since May 23, 2019
 {'scnl':'BLDW.BHZ.AV.--'	, 'value': 200	}, # arrestor station
 ]
 
 duration  = 5*60 # duration value in seconds
 latency   = 10   # seconds between timestamps and end of data window
-min_sta   = 2    # minimum number of stations for detection
+min_sta   = 3    # minimum number of stations for detection
 taper_val = 5 	 # seconds to taper beginning and end of trace before filtering
 f1		  = 1.0  # minimum frequency for bandpass filter
 f2		  = 5.0  # maximum frequency for bandpass filter
 
-plot_duration=3600
+plot_duration = 3600

6d8d6adb32ce4ff84ebdcc452f20f53077d49873
awech <awech@usgs.gov>
2024-05-06 19:38:33 +0000

    cross-post swarm activity to response channels

--- alarm_codes/Swarm.py
+++ alarm_codes/Swarm.py
@@ -164,6 +164,15 @@ def run_alarm(config, T0):
 		if attachment:
 			os.remove(attachment)
 
+		# Post to dedicated response channels for volcnoes listed in config file
+		if 'mm_response_channels' in dir(config):
+			if swarm.iloc[0].VOLCANO in config.mm_response_channels.keys():
+				config.mattermost_channel_id = config.mm_response_channels[swarm.iloc[0].VOLCANO]
+				utils.post_mattermost(config, subject, message, filename=attachment)
+	
+	utils.icinga2_state(config, state, state_message)
+	
+	return
 
 
 def get_extent(lat0, lon0, dist=20):

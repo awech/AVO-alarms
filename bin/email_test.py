d683c4f7af8b8542e5f3a4f7f80e231aba087e2b
awech <awech@usgs.gov>
2024-08-19 23:23:31 +0000

    Reformat python code and add timestamp to message for testing

--- bin/email_test.py
+++ bin/email_test.py
@@ -4,18 +4,27 @@ import sys
 from obspy import UTCDateTime
 
 import sys
-sys.path.append('/alarms')
+
+sys.path.append("/alarms")
 from alarm_codes import utils
 
-if os.getenv('FROMCRON') == 'yep':
-    file=os.environ['LOGS_DIR']+'/Email_test-'+UTCDateTime.now().strftime('%Y%m%d-%H')+'.out'
-    os.system('touch {}'.format(file))
-    f=open(file,'a')
-    sys.stdout=sys.stderr=f
+if os.getenv("FROMCRON") == "yep":
+    file = (
+        os.environ["LOGS_DIR"]
+        + "/Email_test-"
+        + UTCDateTime.now().strftime("%Y%m%d-%H")
+        + ".out"
+    )
+    os.system("touch {}".format(file))
+    f = open(file, "a")
+    sys.stdout = sys.stderr = f
 
+# message = "Body"
+T0 = UTCDateTime.now() - 3600 * 8
+message = f"{T0.strftime('%Y-%m-%d %H:%M')}"
+print(message)
+subject = "Alarm Email Test"
+attachment = os.environ["HOME_DIR"] + "/alarm_aux_files/oops.jpg"
 
-message = 'Body'
-subject = 'Alarm Email Test'
-attachment=os.environ['HOME_DIR']+'/alarm_aux_files/oops.jpg'
-
-utils.send_alert('Error',subject,message,attachment)
+utils.send_alert("Error", subject, message, attachment)
+print("Finished")

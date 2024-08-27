6d68fc3e1319073341ffb6025d6baf00f185a994
awech <awech@usgs.gov>
2024-08-27 00:18:21 +0000

    added AMKA.06 back in

--- alarm_configs/AMKA_Infrasound_config.py
+++ alarm_configs/AMKA_Infrasound_config.py
@@ -8,7 +8,7 @@ SCNL=[
 {'scnl':'AMKA.HDF.AV.03'	, 'sta_lat': 51.378105	, 'sta_lon': 179.301225},
 {'scnl':'AMKA.HDF.AV.04'	, 'sta_lat': 51.37831	, 'sta_lon': 179.30028},
 {'scnl':'AMKA.HDF.AV.05'	, 'sta_lat': 51.379055	, 'sta_lon': 179.30026},
-# {'scnl':'AMKA.HDF.AV.06'	, 'sta_lat': 51.37871	, 'sta_lon': 179.30093},
+{'scnl':'AMKA.HDF.AV.06'	, 'sta_lat': 51.37871	, 'sta_lon': 179.30093},
 ]
 
 # Volcano list to be monitored

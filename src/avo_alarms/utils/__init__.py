2569b40a81742f91356e892d6afbea1412682bff
awech <awech@usgs.gov>
2026-04-13 22:55:44 +0000

    include setup_utils

--- src/avo_alarms/utils/__init__.py
+++ src/avo_alarms/utils/__init__.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from . import messaging
 from . import plotting
 from . import processing
+from . import setup_utils
 
-__all__ = ['messaging', 'plotting', 'processing']
+__all__ = ["messaging", "plotting", "processing", "setup_utils"]

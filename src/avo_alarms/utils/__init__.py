fb21a7282f53e2fd43d4b2e1bad5705a52f5fb4f
awech <awech@usgs.gov>
2026-04-16 20:10:26 +0000

    updated to include `downloading`

--- src/avo_alarms/utils/__init__.py
+++ src/avo_alarms/utils/__init__.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
-from . import messaging, plotting, processing, setup_utils
+from . import messaging, plotting, processing, setup_utils, downloading
 
 load_dotenv()
 plt.style.use(Path(__file__).parent / "./alarms.mplstyle")
 
-__all__ = ["messaging", "plotting", "processing", "setup_utils"]
+__all__ = ["messaging", "plotting", "processing", "setup_utils", "downloading"]
